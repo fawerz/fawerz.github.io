867ff3ad3fbb5521347efdeb55423e58f622f445
Tim <fawerz@icloud.com>
2022-12-19 01:02:32 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-const closeButton = document.querySelector(".search-container .link-close");
-const desktopNav = document.querySelector(".desktop-nav");
-const overlay = document.querySelector(".overlay");
-
-closeButton.addEventListener("click", () => {
-    desktopNav.classList.remove("hide");
-    overlay.classList.remove("show");
-})
-
-overlay.addEventListener("click", () => {
-    desktopNav.classList.remove("hide");
-    overlay.classList.remove("show");
-})
-
-// Mobile Version
+// Mobile Nav
 
 const menuIconContainer = document.querySelector("nav .menu-icon-container");
 const navContainer = document.querySelector(".nav-container");
